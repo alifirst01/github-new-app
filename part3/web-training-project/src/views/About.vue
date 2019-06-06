d76a1fda3be1559e4360038e7c8f3575d1b31fbe
Ali Asgher <ali-asgher@uiowa.edu>
2019-06-06 18:29:54 -0500

    Displayed helloWorld component in a About.vue

--- part3/web-training-project/src/views/About.vue
+++ part3/web-training-project/src/views/About.vue
@@ -1,5 +1,17 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  div(class="about")
+    h1 This is an about page
+    HelloWorld(msg="This is the helloWorld component being used in About component")
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class About extends Vue {}
+</script>
