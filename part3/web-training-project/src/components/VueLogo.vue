4fc067b21009a1108e3ee700b6c1dba575d4961b
Ali Asgher <ali-asgher@uiowa.edu>
2019-06-06 20:22:07 -0500

    Created new component for VueLogo image

---
+++ part3/web-training-project/src/components/VueLogo.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+    img(alt="Vue logo" src="../assets/logo.png")
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class VueLogo extends Vue {}
+</script>
--- part3/web-training-project/src/views/Home.vue
+++ part3/web-training-project/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
   div(class="home")
-    img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
+    VueLogo
+    HelloWorld(msg="Welcome to Your New Vue.js + TypeScript App")
+
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import VueLogo from "@/components/VueLogo.vue"
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    VueLogo
   }
 })
 export default class Home extends Vue {}
