47def92f93127f8f0a820a9208c92b49541c7682
Ali Asgher <ali-asgher@uiowa.edu>
2019-06-08 01:58:02 -0500

    Created an function to make Pokemon API call

--- part3/web-training-project/src/views/Home.vue
+++ part3/web-training-project/src/views/Home.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
   div(class="home")
+    h1 h1 Some text
+    h2 h2 Some text
+    .bg-dark
+      h3.white h3 Some text
+    p p Some text
     VueLogo
     HelloWorld(msg="Welcome to Your New Vue.js + TypeScript App")
 
@@ -9,6 +14,20 @@
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import VueLogo from "@/components/VueLogo.vue"
+import axios from 'axios';
+
+var axiosPokemonCall = (endPoint:string) => {
+  var url = 'https://pokeapi.co/api/v2/' + endPoint;
+  axios.get(url).then(response => {
+    console.log('Status: ', response.status);
+    console.log('Data: ', response.data);
+  }).catch(error => {
+    console.log('Could not fetch the resource');
+    console.log('Error: ', error);
+  });
+}
+
+axiosPokemonCall('type/3/');
 
 @Component({
   components: {
