635831c08e59c80aecc2a9b4a20e9ba9aa24037c
Ali Asgher <ali-asgher@uiowa.edu>
2019-06-25 01:17:12 -0500

    Added unit tests for Date Mixin class

--- part3/web-training-project/src/mixins/DateMixin.ts
+++ part3/web-training-project/src/mixins/DateMixin.ts
@@ -7,7 +7,7 @@ export default class DateMixin extends Vue {
     lastUpdated:Date = new Date()
 
     updateTimeDiff(){
-        var seconds = Math.floor((new Date() - this.lastUpdated) / 1000);
+        var seconds = Math.floor((<any>new Date() - <any>this.lastUpdated) / 1000);
         var minutes = Math.floor(seconds / 60);
         var hours = Math.floor(minutes / 60);
     
---
+++ part3/web-training-project/tests/unit/dateMixin_test.spec.ts
@@ -0,0 +1,41 @@
+import DateMixin from "@/mixins/DateMixin";
+
+describe("DateMixin unit tests", () => {
+    describe("updateTimeDiff()", () => {
+        it('should set timeDiff to x hrs ago if lastUpdated is more than x hours ago', () => {
+            let dateMixin: DateMixin = new DateMixin();
+            
+            dateMixin.lastUpdated = new Date();
+            dateMixin.lastUpdated.setHours(dateMixin.lastUpdated.getHours() - 2);
+            
+            dateMixin.updateTimeDiff();
+
+            let expected: string = "2 hrs ago";
+            expect(dateMixin.timeDiff).toEqual(expected);
+        });
+
+        it('should set timeDiff to x mins ago if lastUpdated is more than x mins ago but less than an hour ago', () => {
+            let dateMixin: DateMixin = new DateMixin();
+            
+            dateMixin.lastUpdated = new Date();
+            dateMixin.lastUpdated.setMinutes(dateMixin.lastUpdated.getMinutes() - 59);
+            
+            dateMixin.updateTimeDiff();
+
+            let expected: string = "59 mins ago";
+            expect(dateMixin.timeDiff).toEqual(expected);
+        });
+
+        it('should set timeDiff to now if lastUpdated is less than a minute ago', () => {
+            let dateMixin: DateMixin = new DateMixin();
+            
+            dateMixin.lastUpdated = new Date();
+            dateMixin.lastUpdated.setSeconds(dateMixin.lastUpdated.getSeconds() - 59);
+            
+            dateMixin.updateTimeDiff();
+
+            let expected: string = "now";
+            expect(dateMixin.timeDiff).toEqual(expected);
+        });
+    });
+})
