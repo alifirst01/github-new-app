acfd4ae8d70b1805b2a417afbe78d4794e0b4da9
Ali Asgher <ali-asgher@uiowa.edu>
2019-07-30 17:39:04 -0500

    Created types.ts file to store all symbols

---
+++ part3/web-training-project/src/utils/types.ts
@@ -0,0 +1,9 @@
+export const TYPE = {
+    "TrendingController": Symbol.for("TrendingController"),
+    "IssuesController": Symbol.for("IssuesController"),
+    "GithubAuthController": Symbol.for("GithubAuthController"),
+    "TrendingRepository": Symbol.for("TrendingRepository"),
+    "IssuesRepository": Symbol.for("IssuesRepository"),
+    "GithubAuthRepository": Symbol.for("GithubAuthRepository"),
+    "AxiosInstance": Symbol.for("AxiosInstance"),
+}
